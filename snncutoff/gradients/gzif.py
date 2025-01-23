b4056cd6e6fb535b29cb5f38b7f06f233f6efe71
Dengyu Wu <d.wooo92@gmail.com>
2025-01-23 23:42:43 +0800

    chores: code polish

--- snncutoff/gradients/gzif.py
+++ snncutoff/gradients/gzif.py
@@ -2,17 +2,6 @@ import torch
 from torch.autograd import Function
 
 class GZIF(Function):
-    # @staticmethod
-    # def forward(ctx, input):
-    #     return input.floor()
-
-    # @staticmethod
-    # def backward(ctx, grad_output):
-    #     if grad_output.min()<-1:
-    #         print(grad_output.min(),grad_output.max())
-    #     return grad_output
-
-
     @staticmethod
     def forward(ctx, input, L):
         L = torch.tensor(L)
@@ -25,11 +14,6 @@ class GZIF(Function):
         (input, out,L) = ctx.saved_tensors
         
         grad_input = grad_output.clone()
-        lower_bound = 0.5 
-        upper_bound = (L + 0.5) 
-        # Condition for m to be within the bounds
-        # in_bounds = (lower_bound <= input) & (input <= upper_bound)
-        # gradient = in_bounds.float()
 
         # Efficiently compute the gradient using a single expression
         gradient = (input * (input < 1.0).float()) + (1.0 * ((input >= 1.0) & (input <= L)).float()) + ((1 - (input - L)) * (input > L).float())
